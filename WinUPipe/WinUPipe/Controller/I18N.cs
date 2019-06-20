88648531d50e4da1df402e3fbf211df97289215c
UPipe-Tech <youpipe1988@gmail.com>
2019-06-20 12:15:16 +0800

    init framework

---
+++ WinUPipe/WinUPipe/Controller/I18N.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+
+/**
+ * ┌───────────────────────────────────────────────────────────────────────┐
+ * │Project	: UTech.Controller
+ * │Comment	:
+ * │
+ * │Version	: V1.0.0.0
+ * │Author		: 
+ * │CreatTime	: 2019/6/20 11:12:32													
+ * ├───────────────────────────────────────────────────────────────────────┤
+ * │Copyright © UTech Team 2015-2019. All rights reserved.
+ * └───────────────────────────────────────────────────────────────────────┘
+ */
+
+
+namespace UTech.Controller
+{
+    
+    using UTech.Properties;
+    /// <summary>
+    /// 處理國際化
+    /// </summary>
+    public static class I18N
+    {
+        private static Dictionary<string, string> _strs = new Dictionary<string, string>();
+
+
+        private static void  Initial(string res)
+        {
+            using(var sr = new StringReader(res))
+            {
+                foreach(var line in sr.NonWhiteSpaceLines())
+                {
+
+                }
+            }
+
+        }
+    }
+}
